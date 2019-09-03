599f672e2703c009f3b0a2ee8ce008e4bd11d887
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-09-02 23:34:46 -0300

    Team controller and initial CRUD

--- backend/app/Controllers/Http/TeamController.js
+++ backend/app/Controllers/Http/TeamController.js
@@ -1,92 +1,44 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with teams
- */
 class TeamController {
-  /**
-   * Show a list of all teams.
-   * GET teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+  async index ({ auth }) {
+    const teams = await auth.user.teams().fetch()
+
+    return teams
   }
 
-  /**
-   * Render a form to be used for creating a new team.
-   * GET teams/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store ({ request, auth }) {
+    const data = request.only(['name'])
+
+    const team = await auth.user.teams().create({
+      ...data,
+      user_id: auth.user.id
+    })
+
+    return team
   }
 
-  /**
-   * Create/save a new team.
-   * POST teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async show ({ params, auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first()
+
+    return team
   }
 
-  /**
-   * Display a single team.
-   * GET teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+  async update ({ params, request, auth }) {
+    const data = request.only(['name'])
+    const team = await auth.user.teams().where('teams.id', params.id).first()
+
+    team.merge(data)
+
+    await team.save()
+
+    return team
   }
 
-  /**
-   * Render a form to update an existing team.
-   * GET teams/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+  async destroy ({ params, auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first()
 
-  /**
-   * Update team details.
-   * PUT or PATCH teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a team with id.
-   * DELETE teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+    await team.delete()
   }
 }
 
