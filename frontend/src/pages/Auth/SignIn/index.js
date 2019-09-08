826c10a09aabea8c61bb24b972c89f309d851e41
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-09-08 10:48:53 -0300

    SignIn page styles

--- frontend/src/pages/Auth/SignIn/index.js
+++ frontend/src/pages/Auth/SignIn/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const SignIn = () => <div>SignIn</div>;
+import Button from '~/styles/components/Button';
+import { Container, SignForm } from '../styles';
+
+const SignIn = () => (
+  <Container>
+    <SignForm onSubmit={() => {}}>
+      <h1>Welcome</h1>
+
+      <span>E-MAIL</span>
+      <input type="email" name="email" />
+
+      <span>PASSWORD</span>
+      <input type="password" name="password" />
+
+      <Button size="big" type="submit">Sign In</Button>
+    </SignForm>
+  </Container>
+);
 
 export default SignIn;
