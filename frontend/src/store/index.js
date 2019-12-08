f1d45b3acaf2b4e1dfc38b9b8bb59d5a457cf2a4
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-12-08 18:25:57 +0100

    React router for redirect

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 
+import history from '~/routes/history';
 import rootReducer from './ducks';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware];
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer(history), applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
 
--- frontend/src/store/sagas/auth.js
+++ frontend/src/store/sagas/auth.js
@@ -1,4 +1,5 @@
 import { call, put } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
 import { actions as toastrActions } from 'react-redux-toastr';
 import api from '~/services/api';
 
@@ -11,6 +12,7 @@ export function* signIn({ email, password }) {
     localStorage.setItem('@Omni:token', response.data.token);
 
     yield put(AuthActions.signInSuccess(response.data.token));
+    yield put(push('/'));
   } catch (err) {
     yield put(toastrActions.add({
       type: 'error',
