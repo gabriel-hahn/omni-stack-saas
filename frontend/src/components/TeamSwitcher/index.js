518e17e0225e06487ef14f0d62f3b97140866f3f
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-12-23 22:06:05 +0100

    Teams reducer and showing data

--- frontend/src/components/TeamSwitcher/index.js
+++ frontend/src/components/TeamSwitcher/index.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import TeamsActions from '~/store/ducks/teams';
 
 import { Container, TeamList, Team } from './styles';
 
-export default class TeamSwitcher extends Component {
+class TeamSwitcher extends Component {
+  componentDidMount() {
+    const { getTeamsRequest } = this.props;
+
+    getTeamsRequest();
+  }
+
   render() {
+    const { teams } = this.props;
+
     return (
       <Container>
         <TeamList>
-          <Team>
-            <img alt="Team Gabriel" src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Gabriel" />
-          </Team>
-          <Team>
-            <img alt="Team Gabriel" src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Gabriel" />
-          </Team>
-          <Team>
-            <img alt="Team Gabriel" src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Gabriel" />
-          </Team>
+          {teams.data.map((team) => (
+            <Team key={team.id}>
+              <img alt={team.name} src={`https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=${team.name}`} />
+            </Team>
+          ))}
         </TeamList>
       </Container>
     );
   }
 }
+
+TeamSwitcher.propTypes = {
+  getTeamsRequest: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  teams: state.teams,
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(TeamsActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamSwitcher);
--- frontend/src/store/sagas/index.js
+++ frontend/src/store/sagas/index.js
@@ -3,6 +3,12 @@ import { all, takeLatest } from 'redux-saga/effects';
 import { signIn } from './auth';
 import { AuthTypes } from '../ducks/auth';
 
+import { getTeams } from './teams';
+import { TeamsTypes } from '../ducks/teams';
+
 export default function* rootSaga() {
-  yield all([takeLatest(AuthTypes.SIGN_IN_REQUEST, signIn)]);
+  yield all([
+    takeLatest(AuthTypes.SIGN_IN_REQUEST, signIn),
+    takeLatest(TeamsTypes.GET_TEAMS_REQUEST, getTeams),
+  ]);
 }
