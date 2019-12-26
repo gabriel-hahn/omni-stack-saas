e320bc52d6dff5f6ffe079f960306e517ba4cf8b
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-12-26 22:17:44 +0100

    Get state of current active team

--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -1,31 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Button from '~/styles/components/Button';
 
 import { Container, Project } from './styles';
 
-const Projects = () => (
-  <Container>
-    <header>
-      <h1>Gabriel Project</h1>
-      <div>
-        <Button onClick={() => { }}>+ New</Button>
-        <Button onClick={() => { }}>Members</Button>
-      </div>
-    </header>
+const Projects = ({ activeTeam }) => {
+  if (!activeTeam) return null;
 
-    <Project>
-      <p>Project example</p>
-    </Project>
+  return (
+    <Container>
+      <header>
+        <h1>{activeTeam.name}</h1>
+        <div>
+          <Button onClick={() => { }}>+ New</Button>
+          <Button onClick={() => { }}>Members</Button>
+        </div>
+      </header>
 
-    <Project>
-      <p>Project example</p>
-    </Project>
+      <Project>
+        <p>Project example</p>
+      </Project>
 
-    <Project>
-      <p>Project example</p>
-    </Project>
-  </Container>
-);
+      <Project>
+        <p>Project example</p>
+      </Project>
 
-export default Projects;
+      <Project>
+        <p>Project example</p>
+      </Project>
+    </Container>
+  );
+};
+
+Projects.propTypes = {
+  activeTeam: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  activeTeam: state.teams.active,
+});
+
+export default connect(mapStateToProps)(Projects);
