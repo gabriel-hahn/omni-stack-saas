031d1cdf99ab26b869977da55d4b371d9a7bc0ac
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-01-02 21:54:22 +0100

    Initial login methods

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   View,
@@ -11,37 +11,59 @@ import {
 
 import styles from './styles';
 
-const SignIn = () => (
-  <KeyboardAvoidingView
-    behavior={Platform.OS === 'ios' ? 'padding' : null}
-    style={styles.container}
-  >
-    <View>
-      <Text style={styles.title}>Hello again</Text>
-      <Text style={styles.label}>E-MAIL</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-        underlineColorAndroid="transparent"
-        returnKeyType="next"
-        autoFocus
-      />
-      <Text style={styles.label}>PASSWORD</Text>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        underlineColorAndroid="transparent"
-        returnKeyType="send"
-        secureTextEntry
-      />
-      <TouchableOpacity onPress={() => { }} style={styles.button}>
-        <Text style={styles.buttonText}>Log in</Text>
-      </TouchableOpacity>
-    </View>
-  </KeyboardAvoidingView>
-);
+export default class SignIn extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
 
-export default SignIn;
+  handleSubmit = () => {
+    const { email, password } = this.state;
+
+    // Action
+  }
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={styles.container}
+      >
+        <View>
+          <Text style={styles.title}>Hello again</Text>
+          <Text style={styles.label}>E-MAIL</Text>
+          <TextInput
+            value={email}
+            onChangeText={text => this.setState({ email: text })}
+            style={styles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            returnKeyType="next"
+            autoFocus
+            onSubmitEditing={() => this.passwordInput.focus()}
+          />
+          <Text style={styles.label}>PASSWORD</Text>
+          <TextInput
+            value={password}
+            onChangeText={text => this.setState({ password: text })}
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            returnKeyType="send"
+            secureTextEntry
+            ref={el => this.passwordInput = el}
+            onSubmitEditing={this.handleSubmit}
+          />
+          <TouchableOpacity onPress={this.handleSubmit} style={styles.button}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
