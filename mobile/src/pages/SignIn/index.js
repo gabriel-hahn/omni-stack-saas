4cf2294d22fd765711fe26f7eaebc7d330137f65
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-01-02 21:38:36 +0100

    Log in styles

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import {
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 
 import styles from './styles';
 
-const SignIn = () => <View style={styles.container} />;
+const SignIn = () => (
+  <KeyboardAvoidingView
+    behavior={Platform.OS === 'ios' ? 'padding' : null}
+    style={styles.container}
+  >
+    <View>
+      <Text style={styles.title}>Hello again</Text>
+      <Text style={styles.label}>E-MAIL</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+        underlineColorAndroid="transparent"
+        returnKeyType="next"
+        autoFocus
+      />
+      <Text style={styles.label}>PASSWORD</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        underlineColorAndroid="transparent"
+        returnKeyType="send"
+        secureTextEntry
+      />
+      <TouchableOpacity onPress={() => { }} style={styles.button}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+    </View>
+  </KeyboardAvoidingView>
+);
 
 export default SignIn;
