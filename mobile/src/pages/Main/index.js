6037d1e5694074a2730c8f69733083efc884d44a
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-01-30 16:07:50 -0300

    Initial header styles

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { connect } from 'react-redux';
 
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { styles } from './styles';
 
-const Main = () => <View />;
+const Main = ({ activeTeam }) => (
+  <View style={styles.backgroundWrapper}>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }} onPress={() => { }}>
+          <Icon name="menu" size={24} color="#fff" />
+        </TouchableOpacity>
+        <Text style={styles.teamTitle}>{activeTeam ? activeTeam.name : 'Select a team'}</Text>
+        <TouchableOpacity hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }} onPress={() => { }}>
+          <Icon name="group" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  </View>
+);
 
-export default Main;
+Main.propTypes = {
+  activeTeam: PropTypes.shape({
+    name: PropTypes.string,
+  }),
+};
+
+Main.defaultProps = {
+  activeTeam: null,
+};
+
+const mapStateToProps = state => ({
+  activeTeam: state.team.active,
+});
+
+export default connect(mapStateToProps)(Main);
